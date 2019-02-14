50b6956bd976e4f98575f51bc159e907531b612b
tdipendra <tdipendra9@gmail.com>
2019-02-14 22:44:15 +0545

    this has been intruppted lesson-6 so once again doing

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,7 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">All Questions </div>
+                <div class="card-header">
+                    <div class="d-flex align-items-centre">
+                        <h2>All Questions </h2>
+                        <a href="{{ route('questions.create')  }} " class="btn btn-outline-secondary"> Ask Question </a>
+                    </div>
+                    
+                </div>
 
                     <div class="card-body">
                     @foreach ($questions as $question)
