b4410b52606aa081789f7827bae6c3dcb9546ebf
tdipendra <tdipendra9@gmail.com>
2019-02-16 21:42:58 +0545

    making fake answers

--- app/Answer.php
+++ app/Answer.php
@@ -22,4 +22,17 @@ class Answer extends Model
         return \Parsedown::instance()->text($this->body); 
 
     }
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function($answer){
+            // echo"Answer Created\n";
+            $answer->question->increment('answers_count'); 
+            $answer->question->save(); 
+        });
+        // static::saved(function($answer){
+        //     echo"Answer Saved\n";
+        // });
+    }
 }
--- app/Question.php
+++ app/Question.php
@@ -28,7 +28,7 @@ class Question extends Model
     }
     public function getStatusAttribute()
     {
-        if ($this->answers_count > 0 ){
+        if ($this->answers_count  > 0 ){
             if ($this->best_answer_id){
                  return "answered-accepted";
             }
